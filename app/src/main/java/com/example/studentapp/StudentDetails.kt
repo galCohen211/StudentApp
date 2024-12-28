be604ccf6e34547b29192b2484fbf79b81adf547
oriya_pelossof <pelossoforiya@gmail.com>
2024-12-28 12:12:01 +0200

    Page details is showing

--- app/src/main/java/com/example/studentapp/StudentDetails.kt
+++ app/src/main/java/com/example/studentapp/StudentDetails.kt
@@ -1,20 +1,37 @@
 package com.example.studentapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class StudentDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_student_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val avatarImageView: ImageView = findViewById(R.id.details_student_avatar)
+        val nameTextView: TextView = findViewById(R.id.details_student_name)
+        val idTextView: TextView = findViewById(R.id.details_student_id)
+        val phoneTextView: TextView = findViewById(R.id.details_student_phone)
+        val addressTextView: TextView = findViewById(R.id.details_student_address)
+        val studentCheckBox: CheckBox = findViewById(R.id.details_student_checkbox)
+
+        // Retrieve data from intent
+        val name = intent.getStringExtra("name")
+        val id = intent.getStringExtra("id")
+        val avatarUrl = intent.getStringExtra("avatarUrl")
+        val isChecked = intent.getBooleanExtra("isChecked", false)
+        val phone = intent.getStringExtra("phone")
+        val address = intent.getStringExtra("address")
+
+        // Bind data to views
+        nameTextView.text = name
+        idTextView.text = id
+        phoneTextView.text = phone
+        addressTextView.text = address
+        studentCheckBox.isChecked = isChecked
+        avatarImageView.setImageResource(R.drawable.student) // Use local image
     }
 }
