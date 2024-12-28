023e29c6da5e3658e30d0d4760dfaecbe6054f6a
Gal Cohen <galcohen211@gmail.com>
2024-12-28 11:32:06 +0200

    Add new student

--- app/src/main/java/com/example/studentapp/NewStudent.kt
+++ app/src/main/java/com/example/studentapp/NewStudent.kt
@@ -1,20 +1,50 @@
 package com.example.studentapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class NewStudent : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_new_student)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val saveButton: Button = findViewById(R.id.add_student_save_button)
+        val cancelButton: Button = findViewById(R.id.add_student_cancel_button)
+
+        val name: EditText = findViewById(R.id.new_student_name)
+        val id: EditText = findViewById(R.id.new_student_id)
+        val phone: EditText = findViewById(R.id.new_student_phone)
+        val address: EditText = findViewById(R.id.new_student_address)
+        val checkbox: CheckBox = findViewById(R.id.new_student_checkbox)
+
+        saveButton.setOnClickListener {
+            val name = name.text.toString()
+            val id = id.text.toString()
+            val phone = phone.text.toString()
+            val address = address.text.toString()
+            val isChecked = checkbox.isChecked
+
+            // Pass the data to StudentList
+            val resultIntent = Intent().apply {
+                putExtra("name", name)
+                putExtra("id", id)
+                putExtra("phone", phone)
+                putExtra("address", address)
+                putExtra("isChecked", isChecked)
+            }
+
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+
+        cancelButton.setOnClickListener {
+            // Close the activity
+            finish()
         }
     }
 }
